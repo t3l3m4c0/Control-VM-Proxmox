5c5c55984b0a37e2528613a82c5f1eac19472b85
t3l3m4c0 <64240412+t3l3m4c0@users.noreply.github.com>
2023-02-16 03:46:52 +0100

    Create control.sh

---
+++ control.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#En las variables definimos cual es el ID de la VM de proxmox que queremos vigilar y el nombre.
+
+numerovm="101";
+nombrevm="W10";
+
+while true; do
+estado=$(qm list | grep "$numerovm")
+if [[ "$estado" = *"$numerovm"*"$nombrevm"*"running"* ]]; then
+#echo "ok $estado"
+echo " "
+else
+fecha=$(date)
+echo "$fecha Se detecta VM $numerovm $nombrevm apagada" >> /root/control/log.txt
+cat /root/control/log.txt | tail -1
+qm start $numerovm
+fi
+clear
+sleep 30
+done
